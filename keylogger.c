214d35923267502a4ed0db410492079d6a3d36a8
Flo Kempenich <flo@kempenich.dev>
2023-09-01 11:33:43 +0100

    Tweak output for my needs (letter frequency)

--- keylogger.c
+++ keylogger.c
@@ -44,7 +44,7 @@ int main(int argc, const char *argv[]) {
     }
 
     // Output to logfile.
-    fprintf(logfile, "\n\nKeylogging has begun.\n%s\n", asctime(localtime(&result)));
+    fprintf(logfile, "\n\n------------------------\n%s------------------------\n", asctime(localtime(&result)));
     fflush(logfile);
 
     // Display the location of the logfile and start the loop.
@@ -161,14 +161,14 @@ const char *convertKeyCode(int keyCode, bool shift, bool caps) {
         case 46:  return shift || caps ? "M" : "m";
         case 47:  return shift ? ">" : ".";
         case 50:  return shift ? "~" : "`";
-        case 65:  return "[decimal]";
-        case 67:  return "[asterisk]";
-        case 69:  return "[plus]";
-        case 71:  return "[clear]";
-        case 75:  return "[divide]";
-        case 76:  return "[enter]";
-        case 78:  return "[hyphen]";
-        case 81:  return "[equals]";
+        case 65:  return ""; // [decimal]
+        case 67:  return ""; // [asterisk]
+        case 69:  return ""; // [plus]
+        case 71:  return ""; // [clear]
+        case 75:  return ""; // [divide]
+        case 76:  return ""; // [enter]
+        case 78:  return ""; // [hyphen]
+        case 81:  return ""; // [equals]
         case 82:  return "0";
         case 83:  return "1";
         case 84:  return "2";
@@ -179,54 +179,54 @@ const char *convertKeyCode(int keyCode, bool shift, bool caps) {
         case 89:  return "7";
         case 91:  return "8";
         case 92:  return "9";
-        case 36:  return "[return]";
-        case 48:  return "[tab]";
-        case 49:  return " ";
-        case 51:  return "[del]";
-        case 53:  return "[esc]";
-        case 54:  return "[right-cmd]";
-        case 55:  return "[left-cmd]";
-        case 56:  return "[left-shift]";
-        case 57:  return "[caps]";
-        case 58:  return "[left-option]";
-        case 59:  return "[left-ctrl]";
-        case 60:  return "[right-shift]";
-        case 61:  return "[right-option]";
-        case 62:  return "[right-ctrl]";
-        case 63:  return "[fn]";
-        case 64:  return "[f17]";
-        case 72:  return "[volup]";
-        case 73:  return "[voldown]";
-        case 74:  return "[mute]";
-        case 79:  return "[f18]";
-        case 80:  return "[f19]";
-        case 90:  return "[f20]";
-        case 96:  return "[f5]";
-        case 97:  return "[f6]";
-        case 98:  return "[f7]";
-        case 99:  return "[f3]";
-        case 100: return "[f8]";
-        case 101: return "[f9]";
-        case 103: return "[f11]";
-        case 105: return "[f13]";
-        case 106: return "[f16]";
-        case 107: return "[f14]";
-        case 109: return "[f10]";
-        case 111: return "[f12]";
-        case 113: return "[f15]";
-        case 114: return "[help]";
-        case 115: return "[home]";
-        case 116: return "[pgup]";
-        case 117: return "[fwddel]";
-        case 118: return "[f4]";
-        case 119: return "[end]";
-        case 120: return "[f2]";
-        case 121: return "[pgdown]";
-        case 122: return "[f1]";
-        case 123: return "[left]";
-        case 124: return "[right]";
-        case 125: return "[down]";
-        case 126: return "[up]";
+        case 36:  return ""; // Return
+        case 48:  return ""; // Tab
+        case 49:  return " "; // Space
+        case 51:  return ""; // [del]
+        case 53:  return ""; // [esc]
+        case 54:  return ""; // [right-cmd]
+        case 55:  return ""; // [left-cmd]
+        case 56:  return ""; // [left-shift]
+        case 57:  return ""; // [caps]
+        case 58:  return ""; // [left-option]
+        case 59:  return ""; // [left-ctrl]
+        case 60:  return ""; // [right-shift]
+        case 61:  return ""; // [right-option]
+        case 62:  return ""; // [right-ctrl]
+        case 63:  return ""; // [fn]
+        case 64:  return ""; // [f17]
+        case 72:  return ""; // [volup]
+        case 73:  return ""; // [voldown]
+        case 74:  return ""; // [mute]
+        case 79:  return ""; // [f18]
+        case 80:  return ""; // [f19]
+        case 90:  return ""; // [f20]
+        case 96:  return ""; // [f5]
+        case 97:  return ""; // [f6]
+        case 98:  return ""; // [f7]
+        case 99:  return ""; // [f3]
+        case 100: return ""; // [f8]
+        case 101: return ""; // [f9]
+        case 103: return ""; // [f11]
+        case 105: return ""; // [f13]
+        case 106: return ""; // [f16]
+        case 107: return ""; // [f14]
+        case 109: return ""; // [f10]
+        case 111: return ""; // [f12]
+        case 113: return ""; // [f15]
+        case 114: return ""; // [help]
+        case 115: return ""; // [home]
+        case 116: return ""; // [pgup]
+        case 117: return ""; // [fwddel]
+        case 118: return ""; // [f4]
+        case 119: return ""; // [end]
+        case 120: return ""; // [f2]
+        case 121: return ""; // [pgdown]
+        case 122: return ""; // [f1]
+        case 123: return ""; // [left]
+        case 124: return ""; // [right]
+        case 125: return ""; // [down]
+        case 126: return ""; // [up]
     }
-    return "[unknown]";
+    return ""; // [unknown]
 }
